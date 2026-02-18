356754731ff77dbe1ad75a0f094a281243b41629
dheap3 <davidjamesheap@gmail.com>
2026-02-17 21:52:16 -0700

    added app.jsx and the header and footers. renamed the original index to login.html

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,34 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./app.css";
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div className="candy-app">
+      <header>
+        <h1>
+          <img src="/images/Candy-Land-Logo.png" alt="Candy Land Logo" />
+        </h1>
+        <menu>
+          <li>
+            <a href="login.html">Home</a>
+          </li>
+          <li>
+            <a href="play.html">Play</a>
+          </li>
+          <li>
+            <a href="progress.html">Progress</a>
+          </li>
+          <li>
+            <a href="about.html">About</a>
+          </li>
+        </menu>
+      </header>
+      <hr />
+
+      <hr />
+      <footer>
+        <div>David Heap</div>
+        <a href="https://github.com/dheap3/genius-startup-idea">GitHub Repository</a>
+      </footer>
+    </div>
+  );
 }
