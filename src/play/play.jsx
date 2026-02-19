fec1ac22be97a47ed9cf88ac0c214f9084b7555e
dheap3 <davidjamesheap@gmail.com>
2026-02-19 08:56:58 -0700

    edited image paths hoping to display them

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -9,14 +9,14 @@ export function Play() {
         <span>Uncle Mike</span>
       </div>
       <div id="play-area">
-        <img id="gameboard" src="public/images/Classic-Board-2004.png" alt="Candy Land Board" />
+        <img id="gameboard" src="images/Classic-Board-2004.png" alt="Candy Land Board" />
         <div id="card-area">
           <div className="deck">
             Card Deck
-            <img className="card" src="public/images/cards/Card Back.png" alt="Back of Card Deck" />
+            <img className="card" src="images/cards/Card Back.png" alt="Back of Card Deck" />
           </div>
           <div className="deck">
-            <img className="card" src="public/images/cards/Purple2.png" alt="Purple 2 Card" />
+            <img className="card" src="images/cards/Purple2.png" alt="Purple 2 Card" />
             Current Card
           </div>
         </div>
