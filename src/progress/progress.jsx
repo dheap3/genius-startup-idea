2ecb9ddf2d22bda73cb2d1b75d90576d6e571362
dheap3 <davidjamesheap@gmail.com>
2026-02-19 08:48:13 -0700

    added progress page

--- src/progress/progress.jsx
+++ src/progress/progress.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import "./progress.css";
 
 export function Progress() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>progress displayed here</div>
+    <main>
+      <h1 id="title">Progress</h1>
+      <div className="progress-board">
+        <div id="header-row" className="row">
+          <span>Player</span>
+          <span>Progress Bar</span>
+          <span>Percent</span>
+        </div>
+        <div className="row">
+          <span className="name">Alice</span>
+          <progress value="70" max="100"></progress>
+          <span className="percent">70%</span>
+        </div>
+        <div className="row">
+          <span className="name">Jerry</span>
+          <progress value="30" max="100"></progress>
+          <span className="percent">30%</span>
+        </div>
+        <div className="row">
+          <span className="name">Uncle Mike</span>
+          <progress value="55" max="100"></progress>
+          <span className="percent">55%</span>
+        </div>
+        <div className="row">
+          <span className="name">Susan</span>
+          <progress value="88" max="100"></progress>
+          <span className="percent">88%</span>
+        </div>
+      </div>
     </main>
   );
 }
