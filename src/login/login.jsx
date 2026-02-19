9c510598fef005cc1a13e317aeacbde8f62d2e78
dheap3 <davidjamesheap@gmail.com>
2026-02-19 08:26:22 -0700

    changed some css to match the original login page

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import "./login.css";
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+    <main>
+      <h1 id="title">Login to Candy Land</h1>
+      <form method="get" action="play.html">
+        <input className="input-box" type="text" placeholder="email@email.com"></input>
+        <input className="input-box" type="password" placeholder="password"></input>
+        <div>
+          <button type="submit">Login</button>
+          <button type="submit">Create</button>
+        </div>
+      </form>
     </main>
   );
 }
