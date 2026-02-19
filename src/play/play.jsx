1d50139082e15b965235220911fd8789d3d1c4cc
dheap3 <davidjamesheap@gmail.com>
2026-02-19 08:50:32 -0700

    finished play page

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import "./play.css";
 
 export function Play() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>play displayed here</div>
+    <main>
+      <div className="player-info">
+        <span>Player: </span>
+        <span>Uncle Mike</span>
+      </div>
+      <div id="play-area">
+        <img id="gameboard" src="public/images/Classic-Board-2004.png" alt="Candy Land Board" />
+        <div id="card-area">
+          <div className="deck">
+            Card Deck
+            <img className="card" src="public/images/cards/Card Back.png" alt="Back of Card Deck" />
+          </div>
+          <div className="deck">
+            <img className="card" src="public/images/cards/Purple2.png" alt="Purple 2 Card" />
+            Current Card
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
