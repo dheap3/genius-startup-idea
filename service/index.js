448bd57dd530f39805d2ae03ef1fa9c33eaeded7
dheap3 <davidjamesheap@gmail.com>
2026-03-17 09:47:57 -0600

    fixed some definitions

--- service/index.js
+++ service/index.js
@@ -1,10 +1,29 @@
-const port = process.argv.length > 2 ? process.argv[2] : 4000;
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcryptjs");
 const express = require("express");
 const uuid = require("uuid");
 const app = express();
 app.use(express.static("public"));
+
+// The scores and users are saved in memory and disappear whenever the service is restarted.
+let users = [];
+let scores = [];
+
+// The service port. In production the front-end code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
+// Serve up the front-end static content hosting
+app.use(express.static("public"));
+
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
 
 // CreateAuth a new user
 apiRouter.post("/auth/create", async (req, res) => {
